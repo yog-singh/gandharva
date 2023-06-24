9eabfb590aad85f04950b94371e4da439c5d6beb
Yogesh <yogesh.singh.ams5@gmail.com>
2023-06-25 02:06:53 +0530

    Changelog: *Added* - Support for basic template for status page - Template rendering

--- src/db/db.go
+++ src/db/db.go
@@ -3,10 +3,13 @@ package db
 import (
 	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"github.com/yog-singh/gandharva/src/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
@@ -14,9 +17,22 @@ var DB *gorm.DB
 func ConnectDB(config *config.Config) {
 	var err error
 	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", config.DBUserName, config.DBUserPassword, config.DBHost, config.DBPort, config.DBName)
-	fmt.Println(dsn)
+	fmt.Println("Connecting to db: ", dsn)
 
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dbLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,        // Don't include params in the SQL log
+			Colorful:                  false,       // Disable color
+		},
+	)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: dbLogger,
+	})
 	if err != nil {
 		log.Fatal("Failed to connect to the Database")
 	}
