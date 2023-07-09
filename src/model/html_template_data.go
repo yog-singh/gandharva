3e600cfb52d5c6782dc0806540a59376e4a84f4a
Yogesh <yogesh.singh.ams5@gmail.com>
2023-07-09 10:46:29 +0530

    Modify status page view template Changed: - HTML generation payload - Status page HTML template

--- src/model/html_template_data.go
+++ src/model/html_template_data.go
@@ -1,8 +1,43 @@
 package model
 
-import "github.com/yog-singh/gandharva/src/entity"
+import (
+	"github.com/yog-singh/gandharva/src/entity"
+)
 
-type HTMLTemplateData struct {
-	BaseURL   string
-	Resources []entity.Resource
+type StatusPageHTMLPayload struct {
+	BaseURL                    string
+	ResourcesUpCount           int32
+	ResourcesDownCount         int32
+	ResourcesInaccessibleCount int32
+	ResourcesUp                []entity.Resource
+	ResourcesDown              []entity.Resource
+	ResourcesInaccessible      []entity.Resource
+}
+
+func GenerateStatusPageHTMLPayload(baseUrl string, resources []entity.Resource) StatusPageHTMLPayload {
+	var resourcesUpCount, resourcesDownCount, resourcesInaccessibleCount int32
+	var resourcesUp, resourcesDown, resourcesInaccessible []entity.Resource
+	for _, resource := range resources {
+		switch resource.Status {
+		case entity.RESOURCE_UP, entity.PENDING:
+			resourcesUpCount = resourcesUpCount + 1
+			resourcesUp = append(resourcesUp, resource)
+		case entity.RESOURCE_DOWN:
+			resourcesDownCount = resourcesDownCount + 1
+			resourcesDown = append(resourcesDown, resource)
+		case entity.RESOURCE_INACCESSIBLE:
+			resourcesInaccessibleCount = resourcesInaccessibleCount + 1
+			resourcesInaccessible = append(resourcesInaccessible, resource)
+		}
+	}
+	statusPagePayload := StatusPageHTMLPayload{
+		BaseURL:                    baseUrl,
+		ResourcesUpCount:           resourcesUpCount,
+		ResourcesDownCount:         resourcesDownCount,
+		ResourcesInaccessibleCount: resourcesInaccessibleCount,
+		ResourcesUp:                resourcesUp,
+		ResourcesDown:              resourcesDown,
+		ResourcesInaccessible:      resourcesInaccessible,
+	}
+	return statusPagePayload
 }
